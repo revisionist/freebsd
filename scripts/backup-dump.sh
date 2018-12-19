4ad67a41469e964f34e02bfb0bbb87c6548798e6
David Goddard <dmg@giles.hq.parse.net>
2018-12-19 22:03:09 +0100

    Substitute root filename

--- scripts/backup-dump.sh
+++ scripts/backup-dump.sh
@@ -164,6 +164,9 @@ backup_filesystem () {
 
   filesystem_name_safe=${filesystem//\//-}
   #echo "filesystem name: "$filesystem_name_safe
+  if [ $filesystem_name_safe == '-' ]; then
+    filesystem_name_safe="root"
+  fi
 
   this_dump_name='backup-level-'${level}
   prior_dump_name=''
