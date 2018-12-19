650b151335c53cce6c5e09c0f421b78dfb0e767b
David Goddard <goddard@acm.org>
2018-12-19 10:40:48 +0100

    Initial commit (not working yet) of dump variant

---
+++ scripts/backup-dump.sh
@@ -0,0 +1,209 @@
+#!/usr/local/bin/bash
+
+# $Id: $
+
+# Backup script written by David Goddard for FreeBSD systems using Dump
+
+DATE=`date +%Y.%m.%d`
+DATE_LONG=`date +%Y%m%d-%H%M`
+DATE_LONG2=`date`
+DEFAULT_BACKUPDIR='/backup/dump'
+DEFAULT_LOGFILE='/var/log/backup-dump.log'
+HOST=`hostname -s`
+FILE_SUFFIX=".dmp"
+DEEPEST_LEVEL=5
+
+
+main() {
+
+  backup_dir=${DEFAULT_BACKUPDIR}
+  logfile=${DEFAULT_LOGFILE}
+  level=-1
+  filesystem=""
+  umask 026
+
+  OPTIND=1
+  while getopts "h?l:d:t:o:" opt; do
+      case "$opt" in
+      h|\?)
+          show_help
+          exit 0
+          ;;
+      l)  level=$OPTARG
+          ;;
+      t)  backup_dir=$OPTARG
+          ;;
+      o)  logfile=$OPTARG
+          ;;
+      esac
+  done
+
+  shift $((OPTIND-1))
+  [ "${1:-}" = "--" ] && shift
+
+  filesystems=("$@")
+  filesystem_count=${#filesystems[@]}
+
+  if [ ${filesystem_count} -eq 0 ]; then
+    echo 'Please specify at least one  filesystem to back up' 
+    exit 1
+  fi
+
+  if [[ ! $level =~ ^-?[0-9]+$ ]]; then
+    echo "Level must be an integer"
+    exit 1
+  fi
+
+  if [ $level -lt 0 ]; then
+    echo 'Please specify a level in range 0 to '${DEEPEST_LEVEL}
+    exit 1
+  fi
+
+  if [ $level -gt $DEEPEST_LEVEL ]; then
+    echo 'Maximum level is: '$DEEPEST_LEVEL
+    exit 1
+  fi
+
+  if [ ! -d $backup_dir ]; then
+    echo 'Backup directory does not exist: '$backup_dir
+   # exit 1
+  fi
+
+  if [ ! -w $backup_dir ]; then
+    echo 'Backup directory is not writeable: '$backup_dir
+    exit 1
+  fi
+
+  echo "Will perform level ${level} backups of ${filesystem_count} filesystems: "${filesystems[*]}
+
+  for ds in "${filesystems[@]}"
+  do
+    backup_filesystem $ds
+  done
+
+  echo "Processed ${filesystem_count} filesystems: "${filesystems[*]}
+
+}
+
+
+show_help () {
+
+  usage="
+  $(basename "$0") [-h] [-t] [-o] -l <filesystems> -- incrementally back up ZFS filesystems to compressed files
+
+  where:
+    -h  show this help text
+    -l  backup level (in range 0-${DEEPEST_LEVEL})
+    -t  directory to output backups to (default: ${DEFAULT_BACKUPDIR})
+    -o  log file to write to (default: ${DEFAULT_LOGFILE})
+
+    <filesystems> - list of ZFS data sets to back up, separated by spaces
+
+  For example:
+
+    $(basename "$0") -l 1 zroot/foo zroot/bar
+
+     - level 1 backup of zroot/foo and zroot/bar
+
+  Works by creating snapshots and sending differentials to file; a full
+  (non-differential) backup is level 0.  Higher levels will be the difference
+  between that level and the previous (i.e. level 2 is difference between
+  snapshots for level 1 and level 2).
+
+  Old backup files and snapshots matching same or higher level will be deleted.
+
+  Ensure that you have taken a level 0 backup before running level 1, level 1
+  before level 2, level 2 before level 3 and so on.
+  "
+
+  echo "$usage"
+
+}
+
+backup_filesystem () {
+
+  filesystem=$1
+
+  filesystem_test=`zfs list | cut -d ' ' -f 1 | grep -c \^${filesystem}\$`
+  if [ $filesystem_test -eq 0 ]; then
+    echo 'Target filesystem does not exist: '$filesystem
+    return 1
+  fi
+
+  echo "Performing level ${level} dump of filesystem "${filesystem}" at "${DATE_LONG2}
+
+  # Enter start log entry:
+  echo `date +%Y-%m-%d`" "`date +%T`" - dump - S - level "${level}" - "${filesystem} >> ${logfile}
+
+  filesystem_name_safe=${filesystem//\//-}
+  #echo "filesystem name: "$filesystem_name_safe
+
+  this_snapshot_name='backup-level-'${level}
+  prior_snapshot_name=''
+
+  declare -a snapshots_to_delete
+  for (( lev=$DEEPEST_LEVEL; lev>=($level-1); lev-- ))
+  do
+    this_name='backup-level-'${lev}
+    if [ $lev -lt $level ]; then
+      if [ $level -eq 0 ]; then
+        prior_snapshot_name=''  # special case
+      else
+        prior_snapshot_name=${this_name}
+      fi
+    else
+      snapshots_to_delete+=(${this_name})
+    fi
+  done
+
+  echo "Will delete these snapshots if they exist: "${snapshots_to_delete[*]}
+  echo "Will create snapshot: "${this_snapshot_name}
+  if [ $level -eq 0 ]; then
+    echo "Will send full data for level 0"
+  else
+    echo "Will send incremental changes since: "${prior_snapshot_name}
+  fi
+
+  for del in "${snapshots_to_delete[@]}"
+  do
+    exist_count=`zfs list -t snapshot -o name,creation -s creation -r ${filesystem} | tail -1 | cut -d ' ' -f 1 | grep -c \@${del}\$`
+    if [ $exist_count -gt 0 ]; then
+      delete_snapshot=${filesystem}'@'${del}
+      echo "Destroying existing snapshot: $delete_snapshot"
+      zfs destroy $delete_snapshot
+    fi
+  done
+
+  create_snapshot=${filesystem}'@'${this_snapshot_name}
+
+  echo "Creating snapshot: $create_snapshot"
+  zfs snapshot ${create_snapshot}
+
+  # Delete matching and lower level backup files
+  echo "Deleting old backup files..."
+  for (( lev=$DEEPEST_LEVEL; lev>=($level); lev-- ))
+  do
+    deletefilepattern=`echo *'-'$HOST'-'$filesystem_name_safe'-level'$lev$FILE_SUFFIX`
+    #echo "Deleting past files: "$deletefilepattern
+    rm -f ${backup_dir}/${deletefilepattern}
+    rm -f ${backup_dir}/${deletefilepattern}.bz2
+  done
+
+  outputfile=`echo $DATE_LONG'-'$HOST'-'$filesystem_name_safe'-level'$level$FILE_SUFFIX`
+  echo "Sending to new file: "$outputfile".bz2"
+
+  if [ $level -gt 0 ]; then
+    zfs send -i ${filesystem}'@'${prior_snapshot_name} ${filesystem}'@'${this_snapshot_name} | bzip2 > ${backup_dir}/${outputfile}.bz2
+  else
+    zfs send ${filesystem}'@'${this_snapshot_name} | bzip2 > ${backup_dir}/${outputfile}.bz2
+  fi
+
+  # Enter end log entry:
+  echo `date +%Y-%m-%d`" "`date +%T`" - dump - F - level "${level}" - "${filesystem} >> ${logfile}
+
+  echo "Completed level ${level} backup of filesystem "${filesystem}" at "${DATE_LONG2}
+
+}
+
+
+main "$@"
